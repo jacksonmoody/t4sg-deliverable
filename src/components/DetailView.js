a16b11ddbc88b4148906f770f253ae97262c79cc
Jackson <jackson@jacksonmoody.com>
2023-02-05 01:23:07 -0500

    Added robustness checks

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -13,47 +13,59 @@ export default function DetailView({ entries }) {
     /*Call function to get link previews from external API*/
     useEffect(() => {
         async function getLinkPreviews() {
-            const getPreviewResults = await getPreviews(entries);
+            let getPreviewResults = await getPreviews(entries);
+            for (let i = 0; i < getPreviewResults.length; i++) {
+                if (getPreviewResults[i].detail !== undefined) { //The API could not find any information for this link
+                    getPreviewResults[i] = {
+                        title: entries[i].name,
+                        description: entries[i].description,
+                        image: {
+                            url: "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+                        }
+                    }
+                }
+            }
             setPreviews(getPreviewResults);
         }
+
         getLinkPreviews();
     }, [entries])
 
-try {
-    const items = entries.map((entry, index) => (
-        <Grid item xs={12} key={index}>
-            <Box
-                sx={{
-                    p: 2,
-                    bgcolor: 'background.paper',
-                    boxShadow: 3,
-                    gap: 2,
-                }}
-            >
-                {/*If items are not null, display them. Otherwise, display the appropriate error message*/}
-                <Stack direction="row" justifyContent="center" alignItems="center" spacing={20}>
-                    <img src={previews[index] !== undefined || previews[index].hasOwnProperty('image') ? previews[index].image["url"] : "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"} alt={entry.name} width="500" height="auto" />
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        width: "30%",
-                    }}>
-                        <h1 align="center">{previews[index] !== undefined || previews[index].hasOwnProperty('title') ? previews[index].title : "No title available. Check the link again to see if it's valid."}</h1>
-                        <h2 align="center">{previews[index] !== undefined || previews[index].hasOwnProperty('description') ? previews[index].description : "No description available."}</h2>
-                        <Button variant="contained" size="small" href={entry.link} target="_blank" rel="noopener noreferrer">Visit Site</Button>
-                    </Box>
-                </Stack>
-            </Box>
-        </Grid>
-    ))
+    try {
+        const items = entries.map((entry, index) => (
+            <Grid item xs={12} key={index}>
+                <Box
+                    sx={{
+                        p: 2,
+                        bgcolor: 'background.paper',
+                        boxShadow: 3,
+                        gap: 2,
+                    }}
+                >
+                    {/*If items are not null, display them. Otherwise, display the appropriate error message*/}
+                    <Stack direction="row" justifyContent="center" alignItems="center" spacing={20}>
+                        <img src={previews[index] !== undefined && previews[index].hasOwnProperty('image') ? previews[index].image["url"] : "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"} alt={entry.name} width="500" height="auto" />
+                        <Box sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            width: "30%",
+                        }}>
+                            <h1 align="center">{previews[index] !== undefined && previews[index].hasOwnProperty('title') ? previews[index].title : "No title available. Check the link again to see if it's valid."}</h1>
+                            <h2 align="center">{previews[index] !== undefined && previews[index].hasOwnProperty('description') ? previews[index].description : "No description available."}</h2>
+                            <Button variant="contained" size="small" href={entry.link} target="_blank" rel="noopener noreferrer">Visit Site</Button>
+                        </Box>
+                    </Stack>
+                </Box>
+            </Grid>
+        ))
 
-    return (
-        <Grid container spacing={2}>
-            {items}
-        </Grid>
-    )
-} catch (error) {
-    console.log(error);
-    return <h1>Invalid links. Please check database and retry.</h1>;
-}
+        return (
+            <Grid container spacing={2}>
+                {items}
+            </Grid>
+        )
+    } catch (error) {
+        console.log(error);
+        return <h1>Invalid links. Please check database and retry.</h1>;
+    }
 }
